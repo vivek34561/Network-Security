a6f42a827a3655bb9fc1c7be5ab26c455830742a
Vivek Kumar Gupta <vivek2.230104023@iiitbh.ac.in>
2026-01-19 19:15:56 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 import sys
 
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__=='__main__':
@@ -16,5 +17,15 @@ if __name__=='__main__':
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
         
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+        
     except Exception as e:
            raise NetworkSecurityException(e,sys)    
+       
+       
+       
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-
+# these are the artifact entities used in various stages of the ML pipeline
+# these are the things that will be produced as output of various stages
+# and will be consumed by other stages
 @dataclass
 class DataIngestionArtifact:
     trained_file_path:str
@@ -7,4 +9,11 @@ class DataIngestionArtifact:
     
     
 
-    
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str    
